c8982ca80bbe0f04df0e2bec8c6934831d1ae518
Student <example@example.com>
2019-09-10 10:44:25 -0700

    It works with masterPostList

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 import PostList from './components/postlist';
 
 class App extends React.Component {
-  return (
-    <div className="App">
-      <PostList postList={this.state.postList}/>
-    </div>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterPostList: [
+        {name: 'Ben', body: 'Hello world one!', vote: 0, comment: []},
+        {name: 'Tae', body: 'Hello world two!', vote: 0, comment: []},
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <PostList postList={this.state.masterPostList}/>
+      </div>
+    );
+  }
 }
 
 export default App;
