1f6114ea8671301c09a4d487121ff3bcb3cf2402
Student <example@example.com>
2019-09-10 14:58:39 -0700

    It add tentative link for reloading comment page

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,29 +2,34 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 class Comments extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
+  }
 
   render(){
     console.log(this.props);
-    const { post } = this.props.location.state
-    // let post = { name: 'Ben', body: 'Hello', vote: 0, comments: [ {name:'Tae', body:'Static first'}, {name:'Tae', body:'Dynamic second'}]};
-
-    return(
-      <div>
-      <h1>Comments</h1>
-      <Link to='/'>Home</Link>
-      <hr />
-      {post.comments.map((comment, i) =>
+    if (this.props.location.state){
+      const { post } = this.props.location.state
+      // let post = { name: 'Ben', body: 'Hello', vote: 0, comments: [ {name:'Tae', body:'Static first'}, {name:'Tae', body:'Dynamic second'}]};
+      return(
         <div>
+        <h1>Comments</h1>
+        <Link to='/'>Home</Link>
+        <hr />
+        {post.comments.map((comment, i) =>
+          <div>
           <p>{comment.name}</p>
           <p>{comment.body}</p>
           <hr />
+          </div>
+        )}
         </div>
-      )}
-      </div>
-    );
+      );
+    } else {
+      return(
+        <Link to='/'>Home</Link>
+      );
+    }
   }
 }
 
